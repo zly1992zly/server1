0124938f9fdb0793692771c4905f42d729a1330c
zly <zly1992zly@126.com>
2019-03-22 09:27:08 +0800

    注册中心1

---
+++ src/main/java/com/baizhi/Server1Application.java
@@ -0,0 +1,15 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Server1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Server1Application.class, args);
+    }
+
+}
